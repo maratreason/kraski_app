54094e98ade3ed73c6bf5b73053fa3483918d173
maratreason <maratreason@mail.ru>
2020-12-09 15:35:28 +0300

    r

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 import Footer from "./components/Footer/Footer";
